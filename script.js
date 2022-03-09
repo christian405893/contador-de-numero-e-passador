3fd8ba5a75aff8b9ad6bfd2919427efb51b3cca3
Christian <8698166990c.f.s.b@gmail.com>
2022-03-09 13:34:23 -0300

    09/03/2022

--- script.js
+++ script.js
@@ -4,26 +4,39 @@ function contar() {
     var txtf = document.getElementById('txtf')
     var txtp = document.getElementById('txtp')
     var res = document.getElementById('res')
-
-    res.innerHTML = `Contando`
+   
+     
+    if ( txti.value.length == 0 || txtf.value.length == 0 || txtp.value.length == 0) 
+    { 
+        window.alert('[ERROR] digite certo')
+        res.innerHTML = `impossível contar`
+    } 
+    else  
+    {
+    res.innerHTML = `Contando: <br>`
     var ni = Number(txti.value)
     var nf = Number(txtf.value)
     var np = Number(txtp.value)
-   
-     
-    if ( ni.value.length == 0 || Number(nf.value) <= nf || Number(nf.value) < 0) { 
-        document.alert('[ERROR] digite certo')
-        res.innerHTML = `impossível contar`
-    } else if (nf.value.length == 0 || Number(nf.value) < 0){
-        document.alert('[ERROR] digite certo')
-        res.innerHTML = `impossível contar`
-    } else if (np.value.length == 0 || Number(np.value) <0){
-        document.alert('[ERROR] digite certo')
-        res.innerHTML = `impossível contar`
-    } else {
-        for (let c = ni; c < nf; c += np){
-            res.innerHTML += `deu:👉${c}`
-        }
+    if (np <= 0) {
+        window.alert('passo inválido, considerando passo 1')
+        np = 1
+    }
+    if ( ni < nf) 
+    {
+    for(let c = ni; c <= nf; c += np) 
+    {
+    res.innerHTML += ` ${c} \u{1F449} `
     }
 
- }
+    
+        }else {
+            for (let c = ni; c >= nf; c -= np) 
+            {
+                res.innerHTML += ` ${c} \u{1f449} `
+            }
+                
+                res.innerHTML += `\u{1F3C1}`
+
+        } 
+}
+}
